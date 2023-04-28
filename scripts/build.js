8194cbc31e103343ec2b4c2280a7caa0b5791a29
Matthew Wolfe <matthew.n.wolfe@centene.com>
2023-04-28 17:34:37 -0400

    fix with nojekyll

--- scripts/build.js
+++ scripts/build.js
@@ -1,24 +1,10 @@
-const mv = require('mv');
 const touch = require('touch');
-
-function move() {
-  return new Promise((resolve, reject) => {
-    mv('out', 'docs', { mkdirp: true }, (error) => {
-      if (error) {
-        reject(error);
-      }
-
-      resolve();
-    });
-  });
-}
 
 async function createNoJekyll() {
   return touch('docs/.nojekyll');
 }
 
 async function run() {
-  await move();
   await createNoJekyll();
 }
 
