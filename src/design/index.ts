1b88939f79cff1023b37211a986b86ca808c3e6a
Matthew Wolfe <matthew.n.wolfe@centene.com>
2023-04-28 18:05:24 -0400

    remove unused components

--- src/design/index.ts
+++ src/design/index.ts
@@ -1,5 +1,3 @@
 export { Button } from './components/Button';
-export { Card } from './components/Card';
 export { Flex } from './components/Flex';
 export { Link } from './components/Link';
-export { TextField } from './components/TextField';
